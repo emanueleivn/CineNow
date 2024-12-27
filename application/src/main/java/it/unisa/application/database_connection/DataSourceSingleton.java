d76c2ffed287ac92b02b233205401cbf81ca8ede
emanueleivn <e.iovane2@studenti.unisa.it>
2024-12-27 17:30:03 +0100

    Cambiamenti Singleton e MainContext

--- application/src/main/java/it/unisa/application/database_connection/DataSourceSingleton.java
+++ application/src/main/java/it/unisa/application/database_connection/DataSourceSingleton.java
@@ -1,4 +1,29 @@
 package it.unisa.application.database_connection;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class DataSourceSingleton {
+    private static volatile DataSource instance;
+
+    private DataSourceSingleton() {}
+
+    public static DataSource getInstance() {
+        if (instance == null) {
+            synchronized (DataSourceSingleton.class) {
+                if (instance == null) {
+                    try {
+                        Context initCtx = new InitialContext();
+                        Context envCtx = (Context) initCtx.lookup("java:comp/env");
+                        instance = (DataSource) envCtx.lookup("jdbc/CineNowDataSource");
+                    } catch (NamingException e) {
+                        throw new RuntimeException("Error initializing DataSource", e);
+                    }
+                }
+            }
+        }
+        return instance;
+    }
 }
--- application/src/main/java/it/unisa/application/database_connection/MainContext.java
+++ application/src/main/java/it/unisa/application/database_connection/MainContext.java
@@ -1,4 +1,22 @@
 package it.unisa.application.database_connection;
 
-public class MainContext {
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+import javax.sql.DataSource;
+
+@WebListener
+public class MainContext implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        DataSource ds = DataSourceSingleton.getInstance();
+        ServletContext context = sce.getServletContext();
+        context.setAttribute("DataSource", ds);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
 }
