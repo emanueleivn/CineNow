0f26de9cbe593561050c6a959526eb5b9aa25b02
emanueleivn <e.iovane2@studenti.unisa.it>
2024-12-28 13:04:43 +0100

    Definizione entity Cliente-Utente e DAO

--- application/src/main/java/it/unisa/application/model/entity/Cliente.java
+++ application/src/main/java/it/unisa/application/model/entity/Cliente.java
@@ -1,4 +1,52 @@
 package it.unisa.application.model.entity;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente extends Utente{
+    private String nome;
+    private String cognome;
+    private List<Prenotazione> prenotazioni;
+
+    public Cliente() {
+        super();
+        this.prenotazioni=new ArrayList<Prenotazione>();
+    }
+    public Cliente(String email, String password,String nome, String cognome) {
+        super(email,password,"cliente");
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public List<Prenotazione> storicoOrdini() {
+        return prenotazioni;
+    }
+
+    public void setPrenotazioni(List<Prenotazione> prenotazioni) {
+        this.prenotazioni = prenotazioni;
+    }
+
+    /*public void aggiungiOrdine(List<PostoProiezione> posti, Proiezione proiezione) {
+        prenotazioni.add();
+    }
+
+    */
+
+
 }
--- application/src/main/java/it/unisa/application/model/entity/Utente.java
+++ application/src/main/java/it/unisa/application/model/entity/Utente.java
@@ -1,4 +1,55 @@
 package it.unisa.application.model.entity;
 
+import java.util.Objects;
+
 public class Utente {
+    private String email;
+    private String password;
+    private String ruolo;
+
+    public Utente(String email, String password,String ruolo) {
+        this.email = email;
+        this.password = password;
+        this.ruolo = ruolo;
+    }
+
+    public Utente() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRuolo() {
+        return ruolo;
+    }
+
+    public void setRuolo(String ruolo) {
+        this.ruolo = ruolo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Utente utente = (Utente) o;
+        return Objects.equals(email, utente.email) && Objects.equals(password, utente.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, password);
+    }
 }
