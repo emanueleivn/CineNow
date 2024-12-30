bb0452458309200148ad82ecabf85aed5a56330b
Armando233-abc <armandovigliotti5@gmail.com>
2024-12-30 10:18:08 +0100

    metodi DAO

--- application/src/main/java/it/unisa/application/model/dao/SedeDAO.java
+++ application/src/main/java/it/unisa/application/model/dao/SedeDAO.java
@@ -1,46 +1,17 @@
 package it.unisa.application.model.dao;
 
 import it.unisa.application.database_connection.DataSourceSingleton;
-import it.unisa.application.model.entity.Proiezione;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+
+
 
 public class SedeDAO {
-    /*
-    * getProgrammazione() -> List<Proiezione>
-    * */
 
-    public List<Proiezione> getProgrammazione(){
-        List<Proiezione> proiezioni = new ArrayList<Proiezione>();
-        String query = "SELECT * FROM proiezione";
+    public Sale retriveById(int id){
 
-        try (Connection connection = DataSourceSingleton.getInstance().getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)) {
+    }
 
-            while (resultSet.next()) {
-                Proiezione p = new Proiezione();
+    public List<Sade> retriveAll(){
 
-                p.setId(resultSet.getInt("id"));
-                p.setDataProiezione(resultSet.getDate("data").toLocalDate());
-
-                //prova
-                p.setIdFilm(resultSet.getInt("id_film"));
-                p.setIdSala(resultSet.getInt("id_sala"));
-                p.setIdOrario(resultSet.getInt("id_orario"));
-
-                proiezioni.add(p);
-            }
-        }catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-
-        return proiezioni;
     }
 }
