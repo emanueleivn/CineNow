d3044daa433078140d51df08514d8080df4986d3
Armando233-abc <armandovigliotti5@gmail.com>
2024-12-30 13:56:33 +0100

    implementazione retriveByFilm

--- application/src/main/java/it/unisa/application/model/dao/ProiezioneDAO.java
+++ application/src/main/java/it/unisa/application/model/dao/ProiezioneDAO.java
@@ -5,6 +5,8 @@ import it.unisa.application.model.entity.*;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProiezioneDAO {
     private final DataSource ds;
@@ -65,5 +67,40 @@ public class ProiezioneDAO {
         return null;
     }
 
+    public List<Proiezione> retriveByFilm(Film film, Sede sede){
+        String sql = "SELECT * FROM proiezione WHERE id_film = ? AND id_sede = ?";
+        List<Proiezione> proiezioni = new ArrayList<>();
 
+        try (Connection connection = ds.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, film.getId());
+            ps.setInt(2, sede.getId());
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                Proiezione proiezione = new Proiezione();
+                proiezione.setId(rs.getInt("id"));
+
+                proiezione.setFilmProiezione(film);
+                proiezione.setDataProiezione(rs.getDate("data").toLocalDate());
+
+                Sala sala = new Sala();
+                sala.setId(rs.getInt("id_sala"));
+                proiezione.setSalaProiezione(sala);
+
+                Slot slotOrario = new Slot();
+                slotOrario.setId(rs.getInt("id_orario"));
+                proiezione.setOrarioProiezione(slotOrario);
+
+                proiezioni.add(proiezione);
+
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return proiezioni;
+
+    }
 }
