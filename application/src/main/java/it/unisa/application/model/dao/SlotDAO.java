9a83085fe961224a966f816fdaaa117ff4c32670
Armando233-abc <armandovigliotti5@gmail.com>
2024-12-30 15:54:18 +0100

    implementazione di retriveFreeSlot

--- application/src/main/java/it/unisa/application/model/dao/SlotDAO.java
+++ application/src/main/java/it/unisa/application/model/dao/SlotDAO.java
@@ -9,6 +9,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SlotDAO {
     private final DataSource ds;
@@ -62,5 +65,26 @@ public class SlotDAO {
         }
 
         return null;
+    }
+
+    public List<Slot> retriveFreeSlot(LocalDate start, LocalDate end, int salaId){
+        List<Slot> freeSlots = new ArrayList<>();
+        String sql = "SELECT s.* FROM slot s, proiezione p " +
+                "WHERE p.id_sala = ? AND p.data BETWEEN ? AND ? AND s.id != p.id_orario";
+
+        try (Connection connection = ds.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Slot slot = new Slot();
+                slot.setId(rs.getInt("id"));
+                slot.setOraInizio(rs.getTime("ora_inizio"));
+
+                freeSlots.add(slot);
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return freeSlots;
     }
 }
