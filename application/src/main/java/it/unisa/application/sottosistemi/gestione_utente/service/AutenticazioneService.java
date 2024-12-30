77c471f769a3f5b1a5eb1c2797149926fb1eabd2
emanueleivn <e.iovane2@studenti.unisa.it>
2024-12-30 12:26:22 +0100

    Modifica LoginServlet

--- application/src/main/java/it/unisa/application/sottosistemi/gestione_utente/service/AutenticazioneService.java
+++ application/src/main/java/it/unisa/application/sottosistemi/gestione_utente/service/AutenticazioneService.java
@@ -2,30 +2,25 @@ package it.unisa.application.sottosistemi.gestione_utente.service;
 
 import it.unisa.application.model.dao.ClienteDAO;
 import it.unisa.application.model.dao.UtenteDAO;
-import it.unisa.application.model.entity.Cliente;
 import it.unisa.application.model.entity.Utente;
-import it.unisa.application.utilities.EmailValidator;
-import it.unisa.application.utilities.PasswordValidator;
-import it.unisa.application.utilities.ValidateStrategyManager;
 import jakarta.servlet.http.HttpSession;
 
-import java.util.Map;
-
 public class AutenticazioneService {
-    private UtenteDAO utenteDAO;
-    private ClienteDAO clienteDAO;
+    private final UtenteDAO utenteDAO;
+    private final ClienteDAO clienteDAO;
 
     public AutenticazioneService() {
         this.utenteDAO = new UtenteDAO();
         this.clienteDAO = new ClienteDAO();
     }
 
-    public Cliente login(String email, String password) {
+    public Utente login(String email, String password) {
         Utente utente = utenteDAO.retrieveByEmail(email);
         if (utente != null && utente.getPassword().equals(password)) {
-            if ("cliente".equalsIgnoreCase(utente.getRuolo())) {
+            if(utente.getRuolo().equalsIgnoreCase("cliente"))
                 return clienteDAO.retrieveByEmail(email, password);
-            }
+            else
+                return utente;
         }
         return null;
     }
