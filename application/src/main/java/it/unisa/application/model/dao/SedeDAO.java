95550ed9773e56f53b7a85a24111ccdcdd024e12
Armando233-abc <armandovigliotti5@gmail.com>
2024-12-30 10:32:00 +0100

    implementazione retriveById DAO

--- application/src/main/java/it/unisa/application/model/dao/SedeDAO.java
+++ application/src/main/java/it/unisa/application/model/dao/SedeDAO.java
@@ -1,17 +1,43 @@
 package it.unisa.application.model.dao;
 
 import it.unisa.application.database_connection.DataSourceSingleton;
+import it.unisa.application.model.entity.Film;
+import it.unisa.application.model.entity.Sede;
 
-
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class SedeDAO {
+    private final DataSource ds;
 
-    public Sale retriveById(int id){
-
+    public SedeDAO() {
+        this.ds = DataSourceSingleton.getInstance();
     }
 
-    public List<Sade> retriveAll(){
 
+    public Sede retriveById(int id){
+        String sql = "SELECT * FROM sede WHERE id = ?";
+        try (Connection connection = ds.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+                String indirizzo = rs.getString("via") + ", " + rs.getString("città") + ", " + rs.getString("cap");
+                return new Sede(
+                        rs.getInt("id"),
+                        rs.getString("nome"),
+                        indirizzo
+                );
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
