337d417a296d7c83f99027b9454634c1f5705725
Armando233-abc <armandovigliotti5@gmail.com>
2024-12-30 12:24:43 +0100

    implementazione metodo create

--- application/src/main/java/it/unisa/application/model/dao/ProiezioneDAO.java
+++ application/src/main/java/it/unisa/application/model/dao/ProiezioneDAO.java
@@ -2,21 +2,37 @@ package it.unisa.application.model.dao;
 
 import it.unisa.application.database_connection.DataSourceSingleton;
 import it.unisa.application.model.entity.Film;
+import it.unisa.application.model.entity.Prenotazione;
 import it.unisa.application.model.entity.Proiezione;
 import it.unisa.application.model.entity.Slot;
+
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class ProiezioneDAO {
-    private static final String INSERT_PROIEZIONE = "INSERT INTO proiezione(id, data, id_film, id_sala, id_orario) VALUES(?, ?, ?, ?, ?)";
+    private final DataSource ds;
 
-    public Proiezione aggiungiProiezione(Slot s, Date d, Film film){
-        try (Connection connection = DataSourceSingleton.getInstance().getConnection();
+    public ProiezioneDAO() {
+        this.ds = DataSourceSingleton.getInstance();
+    }
+
+    public boolean create(Proiezione proiezione){
+        String INSERT_PROIEZIONE = "INSERT INTO proiezione(id, data, id_film, id_sala, id_orario) VALUES(?, ?, ?, ?, ?)";
+        try (Connection connection = ds.getConnection();
              PreparedStatement ps = connection.prepareStatement(INSERT_PROIEZIONE, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setInt(1, proiezione.getId());
+            ps.setDate(2, proiezione.getDataProiezione());
+            ps.setInt(3, proiezione.getFilmProiezione().getId());
+            ps.setInt(4, proiezione.getSalaProiezione().getId());
+            ps.setInt(5, proiezione.getOrarioProiezione().getId());
 
         }catch (SQLException e) {
             e.printStackTrace();
+            return false;
         }
 
-
+        return false;
     }
+
+
 }
