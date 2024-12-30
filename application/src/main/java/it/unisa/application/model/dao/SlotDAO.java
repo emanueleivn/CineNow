e9fed5fb8e5e0533489ed38efb105b5d33261591
Armando233-abc <armandovigliotti5@gmail.com>
2024-12-30 14:24:03 +0100

    getter and setter per ID

--- application/src/main/java/it/unisa/application/model/dao/SlotDAO.java
+++ application/src/main/java/it/unisa/application/model/dao/SlotDAO.java
@@ -1,4 +1,41 @@
 package it.unisa.application.model.dao;
 
+import it.unisa.application.database_connection.DataSourceSingleton;
+import it.unisa.application.model.entity.Slot;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class SlotDAO {
+    private final DataSource ds;
+
+    public SlotDAO() {
+        this.ds = DataSourceSingleton.getInstance();
+    }
+
+    public Slot retriveById(int id){
+        String sql = "select * from slot where id = ?";
+
+        try (Connection connection = ds.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()){
+                Slot slot = new Slot();
+                slot.setId(rs.getInt("id"));
+                slot.setOraInizio(rs.getTime("ora_inizio"));
+
+                return slot;
+            }
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
